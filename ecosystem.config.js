7df503d0eecf87ed817e073e6272000b1820761b
n0xgg04 <dulieu.vblc@gmail.com>
2025-06-01 16:11:29 +0700

    Update ecosystem configuration to rename application from "deno run all --no-log" to "skin-crawler" and adjust script command

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   apps: [
     {
-      name: "deno run all --no-log",
-      script: "./crawler-linux all --no-log",
+      name: "skin-crawler",
+      script: "deno run all:no-log",
     },
   ],
 };
